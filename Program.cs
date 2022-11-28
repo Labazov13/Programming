d3de45905e6b774bb6568bb16d64fa02ad8c31c1
Nikolay <labazopelastra@gmail.com>
2022-11-28 12:22:47 +0700

    Add a code

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿class Animal {
+    string kindOfAnimal;
+    string name;
+    int numberOfLegs;
+    int height;
+    int length;
+    string color;
+    bool hasTail;
+    bool isMammal;
+    bool spellingCorrect;
+    public static void main() {
+        Animal Barsik = new Animal();
+        Barsik.kindOfAnimal = "Cat";
+        Barsik.name = "Кот Барсик";
+        Barsik.numberOfLegs = 4;
+        Barsik.height = 50;
+        Barsik.length = 110;
+        Barsik.color = "Black";
+        Barsik.hasTail = true;
+        Barsik.isMammal = true;
+    }
+    
+
+}

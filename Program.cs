f1fbc0be6cf6b6da8dafcb8c986e55c556051a0d
Nikolay <labazopelastra@gmail.com>
2022-11-28 21:16:21 +0700

    add to method sayHello

--- Program.cs
+++ Program.cs
@@ -1,9 +1,14 @@
 class Animal { 
     string kindOfAnimal;
     string name;
+
+    public void sayHello(string name){
+        Console.WriteLine("Hello "+name);
+    }
     
     public static void Main() {
         Animal Barsik = new Animal();
+        Barsik.sayHello("Petya");
         Barsik.kindOfAnimal = "Cat";
         Barsik.name = "Кот Барсик";
         

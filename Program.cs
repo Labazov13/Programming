f74546d874b14c75e87b5b2dff01278086e7268e
Nikolay <labazopelastra@gmail.com>
2022-11-28 21:45:01 +0700

    add method pravda

--- Program.cs
+++ Program.cs
@@ -2,6 +2,19 @@ class Animal {
     string kindOfAnimal;
     string name;
 // удалили ветку blackList
+
+public int pravda(int a,int b,int c){
+    int summa;
+    if(a==4){
+        summa=b+c;
+    }
+    else{
+        summa=a+b;
+    }
+    Console.WriteLine(summa);
+    return summa;
+
+}
     public int sum(int a,int b){
         int result=a+b;
         Console.WriteLine(result);
@@ -18,6 +31,7 @@ class Animal {
         Barsik.sayHello("Petya");
         Barsik.kindOfAnimal = "Cat";
         Barsik.name = "Кот Барсик";
+        Barsik.pravda(4,9,6);
         Barsik.sum(2,4);
         Console.WriteLine("кот "+ Barsik.kindOfAnimal+ " кличка "+ Barsik.name);
     }
